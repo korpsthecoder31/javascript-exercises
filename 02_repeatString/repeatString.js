2913d996d1b3fb76df6122de9a222b3effa2b2d1
korpsthecoder31 <korpsthewriter@gmail.com>
2024-09-11 07:12:31 +0800

    Create repeatString function
        - create function that takes string and repeats x number of time
        - works on empty string
        - returns error on negative number
    
        note: use better parameter names

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let mainString = ''
 
+    if (num < 0) {
+        return mainString = 'ERROR'
+    }
+    
+    for (i = 0 ; i < num ; i++) {
+        mainString += string
+    }
+
+    return mainString
 };
 
 // Do not edit below this line
