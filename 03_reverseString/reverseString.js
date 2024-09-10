c9071493b9ed01147808865ce66e6b930fde88fd
korpsthecoder31 <korpsthewriter@gmail.com>
2024-09-11 07:44:13 +0800

    Create reverseString function
        - create function that takes string and returns it in reverse order
        - solution split string to array, reverse, convert back to string
    
        - use .reverse() function next time

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reverseWord = ''
+    let letters = string.split('')
 
+    // below can be condensed using .reverse() method
+
+    for (i = letters.length-1 ; i >= 0 ; i--) { 
+        reverseWord += letters[i]
+    }
+    return reverseWord
 };
+
+// return string.split("").reverse().join(""); - ODIN PROJECT SOLUTION
 
 // Do not edit below this line
 module.exports = reverseString;
