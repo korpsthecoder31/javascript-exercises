f12ab3657be0d12de3df7223adbcab7ecc41d435
korpsthecoder31 <korpsthewriter@gmail.com>
2024-09-12 07:40:19 +0800

    Create function that removes args from array
        - used for... of loop to pass elements into if statement
        - if elements !== args, pushes element into newArray
        - returns array after loop is completed
    
        - could have used forEach()
        - could have used .filter()
    
        notes: took me about 24 hours to figure it out.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,25 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...args) {
+    
+    const newArray = [];
+
+    for (const element of array) {
+        if (!args.includes(element)) {
+            newArray.push(element);
+        }
+    }
+    return newArray;
+
+}
+
+// ODIN PROJECT SOLUTION BELOW
+
+//    array.forEach(element => {
+//      if (!args.includes(element)) {
+//        newArray.push(element)
+//      } 
+//    });
+//    return newArray;
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
