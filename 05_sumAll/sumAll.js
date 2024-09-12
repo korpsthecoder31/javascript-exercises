9fb19138db79a03a6ffbcf35c9e68ebd13727e4b
korpsthecoder31 <korpsthewriter@gmail.com>
2024-09-12 08:37:56 +0800

    Creat function that sums all range of parameters
        - create function that takes a & b parameters
        - checks if even, returns error if falsey
        - checks if number, returns error if falsey
        - checks a and b, assigns lower value to a, higher value to b
        - runs loop, a = index , b = condition, executes sum += i
        - returns final sum

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,40 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+
+    if (a < 0 || b < 0) {
+        return 'ERROR';
+    }
+
+    if (Number.isInteger(a) !== true || Number.isInteger(b) !== true) {
+        return 'ERROR';
+    }
+
+    let sum = 0;
+
+    if (a > b) [a, b] = [b, a];     // provided under solutions of ODIN PROJECT used to swap values
+
+    for (i = a ; i <= b ; i++) {
+        sum += i;
+    }
+    return sum;
+
+// My solution:
+
+//    if (a < b) {
+//            for (i = a ; i <= b ; i++) {
+//                sum += i
+//        }
+//        return sum
+//    } else  for (i = b ; i <= a ; i++) {
+//                sum += i
+//    }   return sum
 
 };
+
+// May also use below to swap out values
+
+//  const temp = min;
+//  min = max;
+//  max = temp;
 
 // Do not edit below this line
 module.exports = sumAll;
