1b2ce033ab285e3d3b9aa348e4537acabf0cd2bb
korpsthecoder31 <korpsthewriter@gmail.com>
2024-09-13 07:59:26 +0800

    Create function that checks if year is a leap year
        - create function that checks weather year is divisible by 4, 400 &
        !100, returns true
    
        - consider next time to assign bolean results to variables in which
        to be compared i.e. let variableName = year % 4 === 0

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,21 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    if (year % 4 !== 0) {
+        return false
+    } else if (year % 400 === 0) {
+        return true
+    } else if (year % 4 === 0 && year % 100 !== 0) {
+        return true
+    } else return false
+    
 };
+      
+// Odin project solution: Consider next time to assign boolean to variables 
+
+// const isYearDivisibleByFour = year % 4 === 0;
+//  const isCentury = year % 100 === 0;
+//  const isYearDivisibleByFourHundred = year % 400 === 0;
+
 
 // Do not edit below this line
 module.exports = leapYears;
