ddc5315cbe6ca2400bbaa6cf9a5a13a0b2aea438
korpsthecoder31 <korpsthewriter@gmail.com>
2024-09-13 08:37:14 +0800

    Create functions that convert temp
        - create function that takes tempC parameter and converts it to F
        - create function that takes tempF parameter and converts it to C
        - apply Math.floor rounding conversion off to nearest one decimal
        - return result

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+
+  let convertedTemp = (tempF - 32) * (5/9)
+
+  return Math.round(convertedTemp * 10) / 10
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+
+  let convertedTemp = tempC * (9/5) + 32
+
+  return Math.round(convertedTemp * 10) / 10
+
 };
 
 // Do not edit below this line
