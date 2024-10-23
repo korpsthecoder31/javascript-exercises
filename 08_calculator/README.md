4f05a33896e878d41619fc872c6322ab120629b4
korpsthecoder31 <korpsthewriter@gmail.com>
2024-10-23 08:14:11 +0800

    feat: create calculator functions

--- 08_calculator/README.md
+++ 08_calculator/README.md
@@ -6,3 +6,8 @@ add, subtract, get the sum, multiply, get the power, and find the factorial
 
 In order to do this please fill out each function with your solution. Make sure to return the value so you can test it in Jest! To see the expected value
 take a look at the spec file that houses the Jest test cases.
+
+korpsthecoder comments:
+
+- learned the format of a single line code for reduce
+- learned Math.pow
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, initalValue) => total + initalValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, initalValue) => total * initalValue, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let total = 1;
+  for (i = a ; i > 0 ; i--) {
+    total *= i
+  }
+  return total
 };
 
 // Do not edit below this line
