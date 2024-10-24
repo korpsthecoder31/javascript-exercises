2e72027d8d781f377a04c66d4d7563e1abebe934
korpsthecoder31 <korpsthewriter@gmail.com>
2024-10-25 07:47:39 +0800

    feat: create function returns fibonacci position

--- 10_fibonacci/README.md
+++ 10_fibonacci/README.md
@@ -10,3 +10,7 @@ Create a function that returns a specific member of the Fibonacci sequence:
 fibonacci(4); // returns the 4th member of the series: 3  (1, 1, 2, 3)
 fibonacci(6); // returns 8
 ```
+korpsthecoder comments:
+
+- different from odin project in which no fibonacci array is being created
+- no new methods applied
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+
+    let array = [];
+    let index = parseInt(num)
+
+    if (index === 0) return 0;
+
+    if (index < 0) return "OOPS";
+    
+    for (i = 0 ; i < index ; i++) {
+        if (i < 2) {
+            array.push(1);
+        }        
+
+        if (i>=2) {
+            array.push((array[i-2]) + (array[i-1]));
+        }
+    }
+
+    return array[index - 1];
 
 };
 
